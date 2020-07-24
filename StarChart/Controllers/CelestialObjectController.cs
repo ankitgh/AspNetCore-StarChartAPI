f2b39fff21f9aa732ee5ac7228fede0e966d143a
Ankit Gupta (AZURE-IDENTITY) <ankgupt@microsoft.com>
2020-07-23 17:37:06 -0700

    getbyId

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,19 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet(Name ="GetById")]
+        public IActionResult GetById(int id)
+        {
+            if (!_context.CelestialObjects.Any(x => x.Id == id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(_context.CelestialObjects.First(x => x.Id == id));
+            }
+
+        }
     }
 }
